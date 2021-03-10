99bd73b3a70a14a822256f371168407503ce897c
Andrii Uhryn <an.ugryn@gmail.com>
2021-03-10 14:11:44 +0200

    Baazaar API fix

--- src/api/common/utils.js
+++ src/api/common/utils.js
@@ -1,19 +1,19 @@
 export const paramsToUrlString = (params) => {
-    let str = "";
+    let str = '';
 
     for (let key in params) {
-        if (str !== "") {
-            str += "&";
+        if (str !== '') {
+            str += '&';
         }
         if (params[key] instanceof Array) {
             for (let i = 0; i < params[key].length; i++) {
-                if (str !== "") {
-                    str += "&";
+                if (str !== '') {
+                    str += '&';
                 }
-                str += key + "=" + encodeURIComponent(params[key][i]);
+                str += key + '=' + encodeURIComponent(params[key][i]);
             }
         } else {
-            str += key + "=" + encodeURIComponent(params[key]);
+            str += key + '=' + encodeURIComponent(params[key]);
         }
     }
 
