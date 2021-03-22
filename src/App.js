05b99211e77486fb1867b6f5d5b2cb61041701bb
Andrii Uhryn <an.ugryn@gmail.com>
2021-03-22 21:18:57 +0200

    General snackbar provider

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import  { Route, Switch } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import MainLayout from './root/MainLayout/MainLayout';
 import GhostExplorer from "./pages/GhostExplorer/GhostExplorer";
+import SnackbarContextProvider from "./contexts/SnackbarContext";
 
 export default function App() {
     return (
         <BrowserRouter>
-            <Switch>
-                <Route exact path='/explorer' component={ GhostExplorer } />
-                <Route path='/' component={ MainLayout } />
-            </Switch>
+            <SnackbarContextProvider>
+                <Switch>
+                    <Route exact path='/explorer' component={ GhostExplorer } />
+                    <Route path='/' component={ MainLayout } />
+                </Switch>
+            </SnackbarContextProvider>
         </BrowserRouter>
     );
 }
