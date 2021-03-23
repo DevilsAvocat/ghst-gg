5e433a22b34eaf8a1787852fbf484bb9f6ab0d21
Andrii Koval <a.dude.koval@gmail.com>
2021-03-23 10:19:57 +0200

    split raffle js for 2 components

--- src/pages/Raffle/Raffle.js
+++ src/pages/Raffle/Raffle.js
@@ -1,13 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {Backdrop, Box, CircularProgress, Container, Grid, TextField, Tooltip, Typography} from '@material-ui/core';
-import classNames from 'classnames';
-import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
-import { useStyles } from './styles';
-import { Helmet } from 'react-helmet';
+import {Backdrop, CircularProgress, Container, Typography} from '@material-ui/core';
+import {Helmet} from 'react-helmet';
 
+import RaffleTable from "./components/RaffleTable";
+import RaffleWearables from "./components/RaffleWearables";
 import {ticketsData} from './ticketsData';
-import ghst from '../../assets/images/ghst-doubleside.gif';
-
+import {useStyles} from './styles';
 
 export default function Raffle() {
     const classes = useStyles();
@@ -59,181 +57,9 @@ export default function Raffle() {
             <Helmet>
                 <title>Raffle #4 Calculator</title>
             </Helmet>
-            <Grid container alignItems='center' justify='space-between' spacing={2} className={classes.row}>
-                <Grid item xs={12}>
-                    <Typography variant='h1' align='center' className={classes.title}>Raffle #4 Calculator</Typography>
-                </Grid>
-                <Grid item xs={12} md={3}>
-                    <Typography variant='h6' className={classes.subtitle}>Your Tickets</Typography>
-                </Grid>
-                <Grid container item spacing={1} xs={12} md={8}>
-                    {
-                        tickets.map((ticket, i) => {
-                            return <Grid item xs={4} sm={true} key={i}>
-                                <TextField
-                                    type='number'
-                                    variant='outlined'
-                                    fullWidth
-                                    className={classNames(classes.input, ticket.type)}
-                                    label={ticket.type}
-                                    onChange={(e) => onFieldChange(e, i)}
-                                />
-                            </Grid>
-                        })
-                    }
-                </Grid>
-            </Grid>
-            <Grid container alignItems='center' justify='space-between' spacing={2} className={classes.row}>
-                <Grid item xs={12} md={3}>
-                    <Typography variant='h6' className={classes.subtitle}>Items in Raffle</Typography>
-                </Grid>
-                <Grid container item spacing={1} xs={12} md={8}>
-                    {
-                        tickets.map((ticket, i) => {
-                            return <Grid item xs={4} sm={true} key={i}>
-                                <Typography
-                                    variant='h6'
-                                    align='center'
-                                    className={classNames(classes.textHighlight, ticket.type)}
-                                >
-                                    {ticket.items}
-                                </Typography>
-                            </Grid>
-                        })
-                    }
-                </Grid>
-            </Grid>
-            <Grid container alignItems='center' justify='space-between' spacing={2} className={classes.row}>
-                <Grid item xs={12} md={3}>
-                    <Tooltip
-                        placement='right'
-                        arrow
-                        enterTouchDelay={0}
-                        title={
-                            <React.Fragment>
-                                <Typography>There is still a time before the ruffle meaning more tickets will be minted</Typography>
-                            </React.Fragment>
-                        }
-                    >
-                        <Typography variant='h6' className={classes.subtitle}>
-                            Tickets Supply
-                            <HelpOutlineIcon fontSize='small' className={classes.subtitleIcon} />
-                        </Typography>
-                        
-                    </Tooltip>
-                </Grid>
-                <Grid container item spacing={1} xs={12} md={8}>
-                    {
-                        tickets.map((ticket, i) => {
-                            return <Grid item xs={4} sm={true} key={i} className={classes.ticketBg}>
-                                <img src={ticket.icon} alt={'ticket-' + ticket.type} />
-                                <Typography
-                                    variant='h6'
-                                    align='center'
-                                    className={classNames(classes.textHighlight, ticket.type)}
-                                >
-                                    {ticket.supply}
-                                </Typography>
-                            </Grid>
-                        })
-                    }
-                </Grid>
-            </Grid>
-            <Grid container alignItems='center' justify='space-between' spacing={2} className={classes.row}>
-                <Grid item xs={12} md={3}>
-                    <Tooltip
-                        placement='right'
-                        arrow
-                        enterTouchDelay={0}
-                        title={
-                            <React.Fragment>
-                                <Typography>Average ticket price on Baazaar</Typography>
-                            </React.Fragment>
-                        }
-                    >
-                        <Typography variant='h6' className={classes.subtitle}>
-                            Baazaar price
-                            <HelpOutlineIcon fontSize='small' className={classes.subtitleIcon} />
-                        </Typography>
-                    </Tooltip>
-                </Grid>
-                <Grid container item spacing={1} xs={12} md={8}>
-                    {
-                        tickets.map((ticket, i) => {
-                            return <Grid item xs={4} sm={true} key={i}>
-                                <Typography
-                                    variant='h6'
-                                    align='center'
-                                    className={classNames(classes.textHighlight, classes.price, ticket.type)}
-                                >
-                                    {ticket.cost}
-                                    <img src={ghst} width='26' alt='GHST Token Icon' />
-                                </Typography>
-                            </Grid>
-                        })
-                    }
-                </Grid>
-            </Grid>
-            <Grid container alignItems='center' justify='space-between' spacing={2} className={classes.row}>
-                <Grid item xs={12} md={3}>
-                    <Tooltip
-                        placement='right'
-                        arrow
-                        enterTouchDelay={0}
-                        title={
-                            <React.Fragment>
-                                <Typography>How many items you will get on average</Typography>
-                            </React.Fragment>
-                        }
-                    >
-                        <Typography variant='h6' className={classes.subtitle}>
-                            Your items
-                            <HelpOutlineIcon fontSize='small' className={classes.subtitleIcon} />
-                        </Typography>
-                    </Tooltip>
-                </Grid>
-                <Grid container item spacing={1} xs={12} md={8}>
-                    {
-                        tickets.map((ticket, i) => {
-                            return <Grid item xs={4} sm={true} key={i} className={classNames(classes.chance, ticket.type, ticket.chance !== 0 ? 'highlighted' : '')}>
-                                <Typography
-                                    variant='h6'
-                                    align='center'
-                                    className={classNames(classes.textHighlight, ticket.type)}
-                                >
-                                    {ticket.chance}
-                                </Typography>
-                            </Grid>
-                        })
-                    }
-                </Grid>
-            </Grid>
-            {
-                tickets.map((ticket, i) => {
-                    if(ticket.chance !== 0) return <Grid container className={classes.row} key={i}>
-                        <Grid item xs={12}>
-                            <Typography className={classNames(classes.textHighlight, ticket.type)}>
-                                {ticket.type} items chance
-                            </Typography>
-                        </Grid>
-                        <Grid item container spacing={2} xs={12}>
-                            {
-                                ticket.wearables.map((wearable) => {
-                                    return <Grid item xs={4} sm={3} md={2}>
-                                        <Box className={classNames(classes.wearable, ticket.type)}>
-                                            <Typography>{wearable.name}</Typography>
-                                            <Typography>Available: {wearable.amount}</Typography>
-                                            <Typography className={classNames(classes.textHighlight, ticket.type)}>10%</Typography>
-                                        </Box>
-                                    </Grid>
-                                })
-                            }
-                        </Grid>
-                    </Grid>
-
-                    return null;
-                })
-            }
+            <Typography variant='h1' align='center' className={classes.title}>Raffle #4 Calculator</Typography>
+            <RaffleTable tickets={tickets} onFieldChange={onFieldChange}/>
+            <RaffleWearables tickets={tickets} />
             <Backdrop className={classes.backdrop} open={backdropIsOpen}>
                 <CircularProgress color="inherit" />
             </Backdrop>
---
+++ src/pages/Raffle/components/RaffleTable.js
@@ -0,0 +1,163 @@
+
+import React from "react";
+import {Grid, TextField, Tooltip, Typography} from "@material-ui/core";
+import classNames from "classnames";
+import {useStyles} from "../styles";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+
+import ghst from "../../../assets/images/ghst-doubleside.gif";
+
+export default function RaffleTable({tickets, onFieldChange}) {
+    const classes = useStyles();
+
+    return (
+        <Grid item>
+            <Grid container alignItems='center' justify='space-between' spacing={2} className={classes.row}>
+                <Grid item xs={12} md={3}>
+                    <Typography variant='h6' className={classes.subtitle}>Your Tickets</Typography>
+                </Grid>
+                <Grid container item spacing={1} xs={12} md={8}>
+                    {
+                        tickets.map((ticket, i) => {
+                            return <Grid item xs={4} sm={true} key={i}>
+                                <TextField
+                                    type='number'
+                                    variant='outlined'
+                                    fullWidth
+                                    className={classNames(classes.input, ticket.type)}
+                                    label={ticket.type}
+                                    onChange={(e) => onFieldChange(e, i)}
+                                />
+                            </Grid>
+                        })
+                    }
+                </Grid>
+            </Grid>
+            <Grid container alignItems='center' justify='space-between' spacing={2} className={classes.row}>
+                <Grid item xs={12} md={3}>
+                    <Typography variant='h6' className={classes.subtitle}>Items in Raffle</Typography>
+                </Grid>
+                <Grid container item spacing={1} xs={12} md={8}>
+                    {
+                        tickets.map((ticket, i) => {
+                            return <Grid item xs={4} sm={true} key={i}>
+                                <Typography
+                                    variant='h6'
+                                    align='center'
+                                    className={classNames(classes.textHighlight, ticket.type)}
+                                >
+                                    {ticket.items}
+                                </Typography>
+                            </Grid>
+                        })
+                    }
+                </Grid>
+            </Grid>
+            <Grid container alignItems='center' justify='space-between' spacing={2} className={classes.row}>
+                <Grid item xs={12} md={3}>
+                    <Tooltip
+                        placement='right'
+                        arrow
+                        enterTouchDelay={0}
+                        title={
+                            <React.Fragment>
+                                <Typography>There is still a time before the ruffle meaning more tickets will be minted</Typography>
+                            </React.Fragment>
+                        }
+                    >
+                        <Typography variant='h6' className={classes.subtitle}>
+                            Tickets Supply
+                            <HelpOutlineIcon fontSize='small' className={classes.subtitleIcon} />
+                        </Typography>
+
+                    </Tooltip>
+                </Grid>
+                <Grid container item spacing={1} xs={12} md={8}>
+                    {
+                        tickets.map((ticket, i) => {
+                            return <Grid item xs={4} sm={true} key={i} className={classes.ticketBg}>
+                                <img src={ticket.icon} alt={'ticket-' + ticket.type} />
+                                <Typography
+                                    variant='h6'
+                                    align='center'
+                                    className={classNames(classes.textHighlight, ticket.type)}
+                                >
+                                    {ticket.supply}
+                                </Typography>
+                            </Grid>
+                        })
+                    }
+                </Grid>
+            </Grid>
+            <Grid container alignItems='center' justify='space-between' spacing={2} className={classes.row}>
+                <Grid item xs={12} md={3}>
+                    <Tooltip
+                        placement='right'
+                        arrow
+                        enterTouchDelay={0}
+                        title={
+                            <React.Fragment>
+                                <Typography>Average ticket price on Baazaar</Typography>
+                            </React.Fragment>
+                        }
+                    >
+                        <Typography variant='h6' className={classes.subtitle}>
+                            Baazaar price
+                            <HelpOutlineIcon fontSize='small' className={classes.subtitleIcon} />
+                        </Typography>
+                    </Tooltip>
+                </Grid>
+                <Grid container item spacing={1} xs={12} md={8}>
+                    {
+                        tickets.map((ticket, i) => {
+                            return <Grid item xs={4} sm={true} key={i}>
+                                <Typography
+                                    variant='h6'
+                                    align='center'
+                                    className={classNames(classes.textHighlight, classes.price, ticket.type)}
+                                >
+                                    {ticket.cost}
+                                    <img src={ghst} width='26' alt='GHST Token Icon' />
+                                </Typography>
+                            </Grid>
+                        })
+                    }
+                </Grid>
+            </Grid>
+            <Grid container alignItems='center' justify='space-between' spacing={2} className={classes.row}>
+                <Grid item xs={12} md={3}>
+                    <Tooltip
+                        placement='right'
+                        arrow
+                        enterTouchDelay={0}
+                        title={
+                            <React.Fragment>
+                                <Typography>How many items you will get on average</Typography>
+                            </React.Fragment>
+                        }
+                    >
+                        <Typography variant='h6' className={classes.subtitle}>
+                            Your items
+                            <HelpOutlineIcon fontSize='small' className={classes.subtitleIcon} />
+                        </Typography>
+                    </Tooltip>
+                </Grid>
+                <Grid container item spacing={1} xs={12} md={8}>
+                    {
+                        tickets.map((ticket, i) => {
+                            return <Grid item xs={4} sm={true} key={i} className={classNames(classes.chance, ticket.type, ticket.chance !== 0 ? 'highlighted' : '')}>
+                                <Typography
+                                    variant='h6'
+                                    align='center'
+                                    className={classNames(classes.textHighlight, ticket.type)}
+                                >
+                                    {ticket.chance}
+                                </Typography>
+                            </Grid>
+                        })
+                    }
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+}
