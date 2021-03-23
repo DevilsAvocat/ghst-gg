dfcc211e614f8acb54a109925d6f31dadb670823
Andrii Koval <a.dude.koval@gmail.com>
2021-03-23 14:21:02 +0200

    changed wearables ui

--- src/pages/Raffle/components/RaffleWearables.js
+++ src/pages/Raffle/components/RaffleWearables.js
@@ -12,41 +12,29 @@ export default function RaffleWearables({tickets}) {
     };
 
     return (
-        <Grid container direction='column-reverse'>
+        <Grid container spacing={2}>
             {
-                tickets.map((ticket, i) => {
-                    if(ticket.chance !== 0) return <Grid container className={classes.row} key={i}>
-                        <Grid item xs={12}>
-                            <Typography variant='h6' className={classNames(classes.textHighlight, classes.wearablesTitle, ticket.type)}>
-                                {ticket.type} chances
-                            </Typography>
-                            <Grid container spacing={2}>
-                                {
-                                    ticket.wearables.map((wearable, i) => {
-                                        return <Grid item xs={6} sm={4} md={2} key={i}>
-                                            <Box className={classNames(classes.wearable, ticket.type, wearable.mystery ? 'mystery' : '')}>
-                                                <img src={getWearableIconPath(wearable.icon)} alt={wearable.name} width={65} height={65} />
-                                                <Typography
-                                                    variant={'subtitle1'}
-                                                    className={classNames(classes.textHighlight, ticket.type)}
-                                                >
-                                                    {wearable.name}
-                                                </Typography>
-                                                <Typography variant='body2'>Available: {wearable.amount}</Typography>
-                                                <Typography
-                                                    variant={'subtitle1'}
-                                                    className={classNames(classes.textHighlight, ticket.type)}
-                                                >
-                                                    {wearable.chance}
-                                                </Typography>
-                                            </Box>
-                                        </Grid>
-                                    })
-                                }
+                tickets.slice(0).reverse().map((ticket, i) => {
+                    if(ticket.chance !== 0) return ticket.wearables.map((wearable, i) => {
+                            return <Grid item xs={6} sm={4} md={2} key={i}>
+                                <Box className={classNames(classes.wearable, ticket.type, wearable.mystery ? 'mystery' : '')}>
+                                    <img src={getWearableIconPath(wearable.icon)} alt={wearable.name} width={65} height={65} />
+                                    <Typography
+                                        variant={'subtitle1'}
+                                        className={classNames(classes.textHighlight, ticket.type)}
+                                    >
+                                        {wearable.name}
+                                    </Typography>
+                                    <Typography variant='body2'>Available: {wearable.amount}</Typography>
+                                    <Typography
+                                        variant={'subtitle1'}
+                                        className={classNames(classes.textHighlight, ticket.type)}
+                                    >
+                                        {wearable.chance}
+                                    </Typography>
+                                </Box>
                             </Grid>
-                        </Grid>
-                    </Grid>
-
+                        })
                     return null;
                 })
             }
