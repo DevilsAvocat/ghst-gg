f87dc6e21cfe7036a21dd0b6b7be99d076ce1957
Andrii Uhryn <an.ugryn@gmail.com>
2021-03-24 12:36:09 +0200

    GraphQL request joining

--- src/api/thegraph.js
+++ src/api/thegraph.js
@@ -7,6 +7,28 @@ var baseUrl = 'https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-cor
         cache: new InMemoryCache()
     });
 
+
+async function graphJoin(queries) {
+    try {
+        const queriesCounter = queries.length,
+            responseArray = [];
+
+        for (let i = 0; i < queriesCounter; i++) {
+            await responseArray.push(
+                await client
+                    .query({
+                        query: gql`${queries[i]}`
+                    })
+            )
+        }
+
+        return responseArray;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     async getData(query) {
@@ -14,5 +36,14 @@ export default {
             .query({
                 query: gql`${query}`
             });
+    },
+
+    async getJoinedData(queries) {
+        return await graphJoin(queries);
     }
 }
+
+
+
+
+
