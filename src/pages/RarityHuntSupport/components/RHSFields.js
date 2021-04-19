ab142ac131200b6ab849910a33864b310e5d6f71
Andrii Koval <a.dude.koval@gmail.com>
2021-04-19 19:46:05 +0300

    improved fields layout

--- src/pages/RarityHuntSupport/components/RHSFields.js
+++ src/pages/RarityHuntSupport/components/RHSFields.js
@@ -29,52 +29,56 @@ export default function RHSFields({validAddresses, loadData}) {
 
     return (
         <Grid container spacing={2} style={{marginBottom: 20}}>
-            {
-                addresses.map((address, i)=>{
-                    return <Grid item xs={3} key={i}>
-                        <TextField
-                            type='text'
-                            variant='outlined'
-                            fullWidth
-                            size={'small'}
-                            label={`address ${i + 1}`}
-                            value={address}
-                            disabled={address === validAddresses[i]}
-                            className={classNames(classes.addressField, `color-${i + 1}`, address === validAddresses[i] && 'highlighted')}
-                            onChange={(event) => {
-                                fillAddress(event.target.value, i);
-                            }}
-                            InputProps={{
-                                endAdornment: <IconButton disabled={addresses.length === 1} size={'small'} onClick={() => deleteField(i)}>
-                                    <Close/>
-                                </IconButton>
-                            }}
-                        />
-                    </Grid>
-                })
-            }
-            <Grid item xs={1} className={classes.buttonsAlignment}>
-                <Button
-                    className={classes.fieldsButton}
-                    disabled={addresses.length > 9}
-                    variant={'outlined'}
-                    color={'primary'}
-                    fullWidth
-                    onClick={addMoreFields}
-                >
-                    Add
-                </Button>
+            <Grid container item spacing={2} xs={9}>
+                {
+                    addresses.map((address, i)=>{
+                        return <Grid item xs={4} key={i}>
+                            <TextField
+                                type='text'
+                                variant='outlined'
+                                fullWidth
+                                size={'small'}
+                                label={`address ${i + 1}`}
+                                value={address}
+                                disabled={address === validAddresses[i]}
+                                className={classNames(classes.addressField, `color-${i + 1}`, address === validAddresses[i] && 'highlighted')}
+                                onChange={(event) => {
+                                    fillAddress(event.target.value, i);
+                                }}
+                                InputProps={{
+                                    endAdornment: <IconButton disabled={addresses.length === 1} size={'small'} onClick={() => deleteField(i)}>
+                                        <Close/>
+                                    </IconButton>
+                                }}
+                            />
+                        </Grid>
+                    })
+                }
             </Grid>
-            <Grid item xs={2}>
-                <Button
-                    className={classes.fieldsButton}
-                    variant={'contained'}
-                    color={'primary'}
-                    fullWidth
-                    onClick={() => loadData(addresses)}
-                >
-                    Fetch data!
-                </Button>
+            <Grid container item xs={3} spacing={2}>
+                <Grid item xs={4} className={classes.buttonsAlignment}>
+                    <Button
+                        className={classes.fieldsButton}
+                        disabled={addresses.length > 9}
+                        variant={'outlined'}
+                        color={'primary'}
+                        fullWidth
+                        onClick={addMoreFields}
+                    >
+                        Add
+                    </Button>
+                </Grid>
+                <Grid item xs={8}>
+                    <Button
+                        className={classes.fieldsButton}
+                        variant={'contained'}
+                        color={'primary'}
+                        fullWidth
+                        onClick={() => loadData(addresses)}
+                    >
+                        Fetch data!
+                    </Button>
+                </Grid>
             </Grid>
         </Grid>
     )
