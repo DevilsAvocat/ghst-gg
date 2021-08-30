5e12b5e78c6f143e0420552d793a578f7f76fadf
Andrii Koval <a.dude.koval@gmail.com>
2021-08-30 23:04:59 +0300

    update social counts

--- src/root/Header/Header.js
+++ src/root/Header/Header.js
@@ -69,19 +69,13 @@ export default function Header() {
                     <Link href='https://discord.gg/NXEEETxSkC' className={classes.socialLink} target='_blank'>
                         <Button className={classes.iconButton} aria-label='add an alarm'>
                             <img src={ discord } alt='' />
-                            <Box component='span' className={classes.iconButtonText}>208</Box>
+                            <Box component='span' className={classes.iconButtonText}>325</Box>
                         </Button>
                     </Link>
-                    <Link href='https://t.me/joinchat/hTAWLbZgrKI4YWJk' className={classes.socialLink} target='_blank'>
-                        <Button className={classes.iconButton} aria-label='add an alarm'>
-                            <TelegramIcon />
-                            <Box component='span' className={classes.iconButtonText}>65</Box>
-                        </Button>
-                    </Link>
-                    <Link href='https://twitter.com/ghst_gg' className={classes.socialLink} target='_blank'>
+                    <Link href='https://twitter.com/orden_gg' className={classes.socialLink} target='_blank'>
                         <Button className={classes.iconButton} aria-label='add an alarm'>
                             <TwitterIcon />
-                            <Box component='span' className={classes.iconButtonText}>159</Box>
+                            <Box component='span' className={classes.iconButtonText}>540</Box>
                         </Button>
                     </Link>
                     <Box className={classes.socialLinkJoin}>
